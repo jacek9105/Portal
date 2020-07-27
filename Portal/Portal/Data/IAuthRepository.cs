13ab0587eba0a91e17593925cb4f40f16bcd0638
jacek9105 <jacek.walerowicz91@gmail.com>
2020-07-27 05:56:51 +0200

    Added authorization repository

---
+++ Portal/Portal/Data/IAuthRepository.cs
@@ -0,0 +1,16 @@
+﻿using Portal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portal.Data
+{
+    public interface IAuthRepository
+    {
+        Task<User> Login(string username, string password);
+        Task<User> Register(User user, string password);
+        Task<bool> UserExist(string username);
+
+    }
+}
